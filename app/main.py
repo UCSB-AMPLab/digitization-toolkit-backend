5a9f5e6c2043581433dde52d98ed234dde2af119
jairomelo <jairoantoniomelo@gmail.com>
2026-05-30 14:52:13 -0700

    feat: enable private network access in CORS middleware

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
+    allow_private_network=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
